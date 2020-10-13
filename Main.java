ca86700829c02f1cc2d6f77e2a81062abc865a93
balez4464 <balez4464@wrdsb.ca>
2020-10-13 15:22:38 +0000

    created a program that gives out the last digit of a number using a method

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 /**
- *
- * @author 
+ *This program out puts the last digit of a number using a method
+ * @author Zachary Balean
  */
 public class Main {
+
+  //method for last digit
+  public static int lastDigit(int number){
+    //integer for last digit
+    int lastNumber = number % 10;
+    //if statement if to write the number not negative if it is
+    if(lastNumber < 0){
+      lastNumber = lastNumber * (-1);
+    }
+    //returns the integer
+    return lastNumber;
+  }
+
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // calls the method
+    int answer = lastDigit(3572);
+    //prints the last digit
+    System.out.println(answer);
+  
   }
 }
